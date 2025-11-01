9ed83b1f98d283530875a74e81146f9ee7afa301
Vanya <i.i.lukyanov@ya.ru>
2025-11-01 12:13:29 +0300

    v3 support

--- background.js
+++ background.js
@@ -1,13 +1,19 @@
 chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
+  function(details) {
     if (details.url.includes("://codeforces.org")) {
       return {
         redirectUrl: details.url.replace("://codeforces.org", "://codeforces.com/codeforces.org")
-      }
+      };
     }
     if (details.url.includes("://userpic.codeforces.org")) {
       return {
         redirectUrl: details.url.replace("://userpic.codeforces.org", "://codeforces.com/userpic.codeforces.org")
-      }
+      };
     }
-  }, { urls: ["*://userpic.codeforces.org/*", "*://codeforces.org/*"], types: ["stylesheet", "script", "image"] }, ["blocking"]);
+  },
+  {
+    urls: ["*://userpic.codeforces.org/*", "*://codeforces.org/*"],
+    types: ["stylesheet", "script", "image"]
+  },
+  ["blocking"]
+);
